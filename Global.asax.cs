373816c7c290f98d8d3651b93f4845c1876b7141
KamleshGanar <kamleshganar@hotmail.com>
2017-09-19 05:09:40 +0530

    Example source code.
    Provides the working example .Net source code

---
+++ Global.asax.cs
@@ -0,0 +1,45 @@
+﻿using AT.Net.Service;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace EmailTest
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            #region [ Email Configuration ]
+            Email.Configuration.Display = "Testing Email";
+            Email.Configuration.Username = ConfigurationManager.AppSettings["SMTP_DEFAULT_USERNAME"];
+            Email.Configuration.From = ConfigurationManager.AppSettings["SMTP_DEFAULT_EMAIL"];
+            Email.Configuration.Password = ConfigurationManager.AppSettings["SMTP_DEFAULT_PASSWORD"];
+            Email.Configuration.Port = int.Parse(ConfigurationManager.AppSettings["SMTP_DEFAULT_PORT"]);
+            Email.Configuration.UseSSL = bool.Parse(ConfigurationManager.AppSettings["SMTP_DEFAULT_USESSL"]);
+            Email.Configuration.SMTP = ConfigurationManager.AppSettings["SMTP_DEFAULT_HOST"];
+            Email.Configuration.UseDefaultCredentials = true;
+            Email.Interval = int.Parse(ConfigurationManager.AppSettings["INTERVAL"]);
+            Email.SendTrials = 10;
+            
+            string _ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            EmailDB.ConnectionString = _ConnectionString;
+            string ErrorMessage = string.Empty;
+            if (EmailDB.CheckConnection(ref ErrorMessage))
+            {
+                EmailDB.ConfigureDatabase();
+            }
+            Email.StartProcess();
+            #endregion
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmailTest.Startup))]
+namespace EmailTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
